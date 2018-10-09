37692cc5c5a1ded1e465a0a27e293eaaa25feafe
Bryan M <janbryanmartirez@gmail.com>
2018-10-09 11:16:12 +0800

    user models

--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 const express = require ('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express ()
+const mongoose = require('mongoose')
+require('dotenv').config() //loads environment variables from .env file into process.env
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.DATABASE)
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+//Models
+const { User } = require('./models/user')
+
+app.post('/api/users/register', (req, res)=> {
+    res.send(200)    
+})
+
+
 const port = process.env.PORT || 3001
 
 app.listen(port, ()=> {
