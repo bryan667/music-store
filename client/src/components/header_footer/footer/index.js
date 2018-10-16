0ce06a5878ae9b0170ff231ab2464347d8e72111
bryan667 <janbryanmartirez@gmail.com>
2018-10-16 15:29:16 +0800

    header

---
+++ client/src/components/header_footer/footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import FontAwesomeIcon from '@fontawesome/react-fontawesome'
+import faCompass from '@fontawesome/fontawesome-free-solid/faCompass'
+import faPhone from '@fontawesome/fontawesome-free-solid/faPhone'
+import faClock from '@fontawesome/fontawesome-free-solid/faClock'
+import faEnvelope from '@fontawesome/fontawesome-free-solid/faEnvelope'
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Footer;
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom'
 
 import Home from './components/home/index'
+import Layout from './hoc/layout'
 
 
 const Routes = () => {
     return (
-        <Switch>
-            <Route path='/' exact component={Home}></Route>
-        </Switch>
+        <Layout>
+            <Switch>
+                <Route path='/' exact component={Home}></Route>
+            </Switch>
+        </Layout>
     )
 }
 
