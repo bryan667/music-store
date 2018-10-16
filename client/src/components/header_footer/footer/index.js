79e4280f406944a75b6b27134d2c4fe09e2f5776
bryan667 <janbryanmartirez@gmail.com>
2018-10-16 15:55:51 +0800

    footer

--- client/src/components/header_footer/footer/index.js
+++ client/src/components/header_footer/footer/index.js
@@ -1,15 +1,72 @@
 import React from 'react'
-import FontAwesomeIcon from '@fontawesome/react-fontawesome'
-import faCompass from '@fontawesome/fontawesome-free-solid/faCompass'
-import faPhone from '@fontawesome/fontawesome-free-solid/faPhone'
-import faClock from '@fontawesome/fontawesome-free-solid/faClock'
-import faEnvelope from '@fontawesome/fontawesome-free-solid/faEnvelope'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faCompass from '@fortawesome/fontawesome-free-solid/faCompass'
+import faPhone from '@fortawesome/fontawesome-free-solid/faPhone'
+import faClock from '@fortawesome/fontawesome-free-solid/faClock'
+import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope'
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className='bck_b_dark'>
+            <div className='container'>
+                <div className='logo'>
+                    Waves
+                </div>
+                <div className='wrapper'>
+                    <div className='left'>
+                        <h2>Contact Information</h2>
+                        <div className='business_nfo'>
+                            <div className='tag'>
+                                <FontAwesomeIcon
+                                    icon={faCompass}
+                                    className='icon'
+                                />
+                                <div className='nfo'>
+                                    <div>Address</div>
+                                    <div>Good Doge 245</div>
+                                </div>
+                            </div>
+                            <div className='tag'>
+                                <FontAwesomeIcon
+                                    icon={faPhone}
+                                    className='icon'
+                                />
+                                <div className='nfo'>
+                                    <div>Phone</div>
+                                    <div>777-1245</div>
+                                </div>
+                            </div>
+                            <div className='tag'>
+                                <FontAwesomeIcon
+                                    icon={faClock}
+                                    className='icon'
+                                />
+                                <div className='nfo'>
+                                    <div>Working Hours</div>
+                                    <div>Mon-Sun / 9am-8pm</div>
+                                </div>
+                            </div>
+                            <div className='tag'>
+                                <FontAwesomeIcon
+                                    icon={faEnvelope}
+                                    className='icon'
+                                />
+                                <div className='nfo'>
+                                    <div>Email</div>
+                                    <div>doge@musicnfo.com</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='right'>
+                        <h2>Git Gud Boi</h2>
+                        <div>
+                            <div>Woof Woof</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- client/src/hoc/layout.js
+++ client/src/hoc/layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/header_footer/header/index'
+import Footer from '../components/header_footer/footer/index'
 
 class Layout extends Component {
     render() {
@@ -9,7 +10,7 @@ class Layout extends Component {
                 <div className='page_container'>
                     {this.props.children}
                 </div>
-                Footer
+                <Footer/>
             </div>
         );
     }
