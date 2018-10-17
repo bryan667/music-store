5534b918cc92821ebcd25ca2c40967118f8579fc
Bryan M <janbryanmartirez@gmail.com>
2018-10-17 11:12:43 +0800

    added regex for email

--- client/src/components/utils/form/formactions.js
+++ client/src/components/utils/form/formactions.js
@@ -2,6 +2,14 @@
 export const validate = (element, formdata=[]) => {
     let error = [true,'']
 
+    if (element.validation.email) {
+        const regex1 = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        const valid = regex1.test(element.value.trim())
+        const message = `${!valid ? 'Must be a valid email' : ''}`
+
+        error = !valid ? [valid, message] : error
+    }
+
     if (element.validation.required) {
         const valid = element.value.trim() !== ''
         const message = `${!valid ? 'This field is required' : ''}`
